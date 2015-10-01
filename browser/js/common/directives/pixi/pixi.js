8e8f52e34bea7d100123c5c01b3fd91da9766951
Aneury Casado <aneury.casado@gmail.com>
2015-09-30 22:21:45 -0400

    Basic board drawn.

--- browser/js/common/directives/pixi/pixi.js
+++ browser/js/common/directives/pixi/pixi.js
@@ -5,25 +5,78 @@ app.directive("pixi", function(){
   }
 });
 
-app.controller("PixiController", function($scope){
-   console.log("Called");
-   var stage = new PIXI.Stage(0x66FF99);
-   var renderer = PIXI.autoDetectRenderer(400, 300);
-   document.body.appendChild(renderer.view);
-   requestAnimationFrame(animate);
-   var texture = PIXI.Texture.fromImage("/js/images/creep/creep-1-blue/1.png");
-    // create a new Sprite using the texture
-    var bunny = new PIXI.Sprite(texture);
-    // center the sprites anchor point
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-    // move the sprite t the center of the screen
-    bunny.position.x = 200;
-    bunny.position.y = 150;
+function Game(rows,cols,towers){
+  this.rows = rows;
+  this.cols = cols;
+  console.log("Width ",window.innerWidth);
+  console.log("Height ", window.innerHeight);
+  var cellWidth = window.innerWidth / cols
+  var cellHeight = window.innerHeight / rows
+  console.log(cellWidth);
+  console.log(cellHeight);
+  var startRow = 1;
+  var startCol = 0;
+  var endRow = rows -2;
+  var endCol = cols -1;
+  // this.towers = createTowers(towers);
+  this.grid = [];
+  for(var col = 0; col < cols; col++){
+    for(var row = 0; row < rows; row++){
+      var imgRoute;
+       var imgRoute = "/js/images/background-tilesets/01.png";
+      if(row === 0){
+        imgRoute = "/js/images/background-tilesets/15.png";
+      }else if(row === rows - 1){
+        imgRoute = "/js/images/background-tilesets/14.png";
+      }else if(col === 0 && row !== startRow){
+        imgRoute = "/js/images/background-tilesets/13.png";
+      }else if(col === cols-1 && row !== endRow){
+        imgRoute = "/js/images/background-tilesets/08.png";
+      }else if(row === startRow && col === startCol){
+        imgRoute = "/js/images/background-tilesets/01.png";
+      }else if(row === endRow && col === endCol){
+        imgRoute = "/js/images/background-tilesets/01.png";
+      }
+      else{
+        imgRoute = "/js/images/background-tilesets/01.png";
+      }
+      var obj = {
+        texture: PIXI.Texture.fromImage(imgRoute),
+        tag: 'land',
+        x: col*(cellWidth-30),
+        y: row*cellHeight
+      }
+      this.grid.push(obj);
+    }
+  }
+}
 
-    stage.addChild(bunny);
-   function animate() {
-       requestAnimationFrame( animate );
-       renderer.render(stage);
+
+app.controller("PixiController", function($scope, $rootScope){
+  if(!$rootScope.called){
+    $rootScope.called = true;
+     console.log("Called");
+     var stage = new PIXI.Stage(0x66FF99);
+     var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+     document.body.appendChild(renderer.view);
+     requestAnimationFrame(animate);
+     var game = new Game(10,10,13);
+     game.grid.forEach(function(cell){
+        var texture = cell.texture;
+        var land = new PIXI.Sprite(texture);
+        console.log("x ",cell.x);
+        console.log("y ", cell.y);
+        // land.anchor.x = 0.5;
+        // land.anchor.y = 0.5;
+        land.position.x = cell.x;
+        land.position.y = cell.y;
+        stage.addChild(land);
+     })
+     function animate() {
+         requestAnimationFrame( animate );
+         renderer.render(stage);
+     }
+   }else{
+     console.log("Sucks");
    }
 })
