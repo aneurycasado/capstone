30ab80f094e59269e4e285b51c99fda9b528eefc
Aneury Casado <aneury.casado@gmail.com>
2015-10-02 03:11:17 -0400

    Set up basic file strucutre for start, and loading states. Also cleaned up the css.

---
+++ browser/js/ng/states/choseMap/choseMap.state.js
@@ -0,0 +1,15 @@
+var app = require("../../app.js");
+app.config(function($stateProvider){
+	$stateProvider
+	.state('choseMap', {
+		url: '/',
+		templateUrl: '/js/ng/states/choseMap/choseMap.state.html',
+		controller: 'ChoseMapController'
+	})
+});
+
+app.controller('ChoseMapController', function($scope, $state){
+	$scope.play = function(){
+		$state.go('play',{num:1})
+	}
+});
