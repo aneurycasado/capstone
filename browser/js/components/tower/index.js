569bce2c7ca9c3f17c1e3880a28823045727c62e
Justin Barabander <jbarabander@gmail.com>
2015-10-02 01:36:27 -0400

    Added some additional tower properties

--- browser/js/components/tower/index.js
+++ browser/js/components/tower/index.js
@@ -6,9 +6,11 @@ class Tower {
     constructor(x,y,options) {
         //this.grid = grid;
         this.position = {x: x, y: y};
+        this.level = 1;
+        this.kills = 0;
         this.options = options ? options : {};
         if(options){
-            if(options.img) this.img = new PIXI.Sprite(PIXI.Texture.fromImage("/images/tower-defense-turrets/turret-" + options.img + ".png"));
+            if(options.img) this.img = new PIXI.Sprite(PIXI.Texture.fromImage("/images/tower-defense-turrets/turret-" + options.img + '-' + this.level + ".png"));
             this.img.position.x = this.position.x * game.cellSize + .5*game.cellSize;
             this.img.anchor.x = .5;
             this.img.anchor.y = .5;
@@ -16,6 +18,11 @@ class Tower {
             game.stages["play"].addChild(this.img);
         }
         this.codeSnippets = [];
+    }
+    addKill() {
+        this.kills++;
+        if(this.kills === 20) this.level = 2;
+        else if(this.kills === 60) this.level = 3;
     }
 }
 
@@ -28,7 +35,14 @@ function createTower(x, y, type) {
 
 class IceTower extends Tower {
     constructor(x, y) {
-        super(x, y, {img: '4-1'});
+        super(x, y, {img: '4'});
+    }
+
+}
+
+class LaserTower extends Tower {
+    constructor(x, y) {
+        super(x, y, {img: '6'})
     }
 }
 
