572c3a82053df23daf85fca1de79d0bb97a0c826
Justin Barabander <jbarabander@gmail.com>
2015-10-01 20:39:45 -0400

    Added directives and states

--- browser/js/ng/app.js
+++ browser/js/ng/app.js
@@ -1,7 +1,7 @@
-//var angular = require('angular');
-//var app = angular.module('TowerDefense', ['ui.router']);
-//app.config(function($locationProvider) {
-//    $locationProvider.html5Mode(true);
-//});
-//
-//module.exports = app;
+var angular = require('angular');
+var app = angular.module('TowerDefense', [require('ui.router')]);
+app.config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
+module.exports = app
+
