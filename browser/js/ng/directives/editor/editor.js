31a777241668414aafda77f9a7941cca5cd43781
Justin Barabander <jbarabander@gmail.com>
2015-10-05 16:26:28 -0400

    Initial Commit

--- browser/js/ng/directives/editor/editor.js
+++ browser/js/ng/directives/editor/editor.js
@@ -16,14 +16,14 @@ app.directive('editor', function() {
             //scope.tower.editSession = editor;
             editor.focus();
             scope.saveCodeSnippet = function() {
-                //if(scope.tower) {
-                //    console.log(scope.tower);
-                //    if(scope.tower.session === null) scope.tower.session = ace.createEditSession('', 'ace/mode/javascript');
-                //    scope.tower.session.setValue(editor.getValue())
-                //    editor.setSession(scope.tower.session);
-                //}
-                scope.tower.codeSnippets.push(editor.getValue());
-                console.log(scope.tower);
+                if(scope.tower) {
+                    console.log(scope.tower);
+                    if(scope.tower.session === null) scope.tower.session = ace.createEditSession('', 'ace/mode/javascript');
+                    scope.tower.session.setValue(editor.getValue())
+                    editor.setSession(scope.tower.session);
+                }
+                scope.tower.codeSnippets = editor.getValue();
+                console.log(scope.tower.session);
                 scope.editing = false;
             }
         }
