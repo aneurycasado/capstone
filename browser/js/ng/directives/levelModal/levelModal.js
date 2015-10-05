42bca9ffb7249cf3d10030338a337cbf04b97060
Justin Barabander <jbarabander@gmail.com>
2015-10-05 02:37:51 -0400

    Got modal window to work

--- browser/js/ng/directives/levelModal/levelModal.js
+++ browser/js/ng/directives/levelModal/levelModal.js
@@ -1,7 +1,7 @@
 app.directive("levelModal", function($rootScope){
     return {
         restrict: "E",
-        templateUrl: "js/ng/directives/startModal/startModal.html",
+        templateUrl: "js/ng/directives/levelModal/levelModal.html",
         link: function(scope){
             $rootScope.$on('wavesDone', function() {
                 $("#levelModal").modal("show");
