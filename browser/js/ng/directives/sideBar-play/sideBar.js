59a1327ff6bc6bd7da5191d198237952eb13201b
Aneury Casado <aneury.casado@gmail.com>
2015-10-10 15:15:54 -0400

    Working on overall stats.

--- browser/js/ng/directives/sideBar-play/sideBar.js
+++ browser/js/ng/directives/sideBar-play/sideBar.js
@@ -84,7 +84,9 @@ app.controller('SideBarPlayController', function($scope, $rootScope, PlayerFacto
         $scope.state = StateFactory.state;
         $scope.totalEnemies = WaveFactory.currentWaveLength();
     }
-
+    $scope.showStats = () => {
+        $("#playerStatisticsModal").modal("toggle");
+    }
     // $scope.initiateLevel = () => {
     //     console.log("Next LEvel");
     // }
