74da756399551b8428d03488aec82a446331c263
Aneury Casado <aneury.casado@gmail.com>
2015-10-10 17:46:50 -0400

    Popover in progress

--- browser/js/ng/directives/towerStore/towerStore.js
+++ browser/js/ng/directives/towerStore/towerStore.js
@@ -1,6 +1,12 @@
 app.directive("towerStore", function(){
     return {
         restrict: "E",
-        templateUrl: 'js/ng/directives/towerStore/towerStore.html'
+        templateUrl: 'js/ng/directives/towerStore/towerStore.html',
+        controller: "TowerStoreController"
     }
+});
+
+app.controller('TowerStoreController', function($scope){
+	console.log("We called TowerStoreController");
+	$('.popOverSideBar').popover();
 });
